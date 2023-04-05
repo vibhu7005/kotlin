94c62e00c6c2bef6a40baa32e6d4bea59cebd192
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-04-06 00:11:47 +0530

    arrays pushed

---
+++ src/main/java/kt/arrays/Arrays.kt
@@ -0,0 +1,16 @@
+package kt.arrays
+
+import java.util.ArrayList
+
+fun main() {
+    val names = arrayOf("z","t","h")
+    for (name in names) {
+        println(name)
+    }
+    val numbers = intArrayOf(2,4,5)
+    val nums = arrayOf(2,5,6)
+  //  val names =
+    StudentNames().printNames(nums.toIntArray())
+    val array = numbers.toTypedArray()
+
+}
---
+++ src/main/java/kt/arrays/StudentNames.java
@@ -0,0 +1,12 @@
+package kt.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StudentNames {
+    void printNames (int array[]) {
+        for (int i= 0; i<array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
