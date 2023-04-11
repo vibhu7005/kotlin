c96698c19aeb8e545b2020508952ec6a9ff85a18
vibhu7005 <vaibhavg7005@gmail.com>
2023-04-12 03:46:18 +0530

    Singleton pushed

---
+++ src/main/java/kt/kotlinobject/Singleton.kt
@@ -0,0 +1,19 @@
+package kt.kotlinobject
+
+fun main() {
+    println(Singleton.getMessage())
+    println(Singleton.getMessage())
+    println(Singleton.getMessage())
+    Singleton.getMessage()
+    Singleton.getMessage()
+    Singleton.getNoOfInvokations()
+}
+
+object Singleton {
+    fun getMessage() = "Object class is a sngleton in kotlin"
+    private var count = 0
+    init {
+        count++
+    }
+    fun getNoOfInvokations() = println("${javaClass.name} is initialized $count times")
+}
