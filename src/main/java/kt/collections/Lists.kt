5c1aa58818439fa5d99bfc0a693d24c3f391988b
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-04-28 15:39:59 +0530

    kotlin collections list

--- src/main/java/kt/collections/Lists.kt
+++ src/main/java/kt/collections/Lists.kt
@@ -14,4 +14,18 @@ fun main() {
     val exList = listOf(*colorArray)
     println(exList)
     println(list2)
+
+    val colorList = listOf("Red", "blue", "Green", "Red")
+    val seasonsList = listOf("summer","winter","rainy", "winter")
+    println(colorList.last())
+    println(colorList.asReversed())
+    println(colorList.getOrNull(5))
+    println(colorList)
+    println(colorList.zip(seasonsList))
+    val combined = listOf(colorList, seasonsList)
+    println(combined)
+    val actualCombined = colorList + seasonsList
+    println(actualCombined)
+    println(colorList.union(seasonsList))
+    println(colorList.distinct())
 }
