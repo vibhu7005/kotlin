c226c794fb399c1ab14d407fe326c0d8a2115c81
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-05-01 08:11:33 +0530

    kotlin collections maps, pairs

--- src/main/java/kt/collections/Maps.kt
+++ src/main/java/kt/collections/Maps.kt
@@ -4,7 +4,8 @@ import java.lang.Exception
 import java.lang.NumberFormatException
 
 fun main() {
-    val map = hashMapOf(1 to Car("Maserati", 2001), 2 to Car("BMW", 2019))
+    val map = mapOf(1 to Car("Maserati", 2001), 2 to Car("BMW", 2019))
+    println(map)
     //way 1
     for (entry in map) {
         println(" ${entry.key} ${entry.value} ")
