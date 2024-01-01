81317c110cbf2f44a3679efa785ba8d0db77192f
vaibhav <vaibhavg7005@gmail.com>
2024-01-01 12:59:21 +0530

    java test only

--- src/main/java/kt/academy/learnprogramming/challenge6/JavaToKotlin/Main.java
+++ src/main/java/kt/academy/learnprogramming/challenge6/JavaToKotlin/Main.java
@@ -8,25 +8,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Main obj = new Main();
 //        Integer x = 4;
 //        obj.calc(x);
 //        System.out.println(x);
-        String s = "abc";
-        StringBuilder sb  = new StringBuilder("xxx");
-        Map<Character, Integer> map = new HashMap();
-//        for (int i = 0; i < s.length(); i++) {
-//            if (map.containsKey(s.charAt(i))) {
-//                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
-//            } else {
-//                map.put(s.charAt(i), 1);
-//            }
-//            sb.append("x");
+//        String s = "abc";
+//        StringBuilder sb  = new StringBuilder("xxx");
+//        Map<Character, Integer> map = new HashMap();
+////        for (int i = 0; i < s.length(); i++) {
+////            if (map.containsKey(s.charAt(i))) {
+////                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+////            } else {
+////                map.put(s.charAt(i), 1);
+////            }
+////            sb.append("x");
+////        }
+//        boolean visited[] = new boolean[3];
+//        obj.subsets(s, 0, sb);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4 * 2 ; i++) {
+            sb.append('x');
+        }
+        obj.generate(sb, 0, 4 * 2);
+//        String ans[] = new String[res.size()];
+//        ArrayList<String> res = new ArrayList<>();
+//        for (int i = 0; i < ans.length; i++) {
+//            ans[i] = res.get(i);
 //        }
-        boolean visited[] = new boolean[3];
-        obj.subsets(s, 0, sb);
     }
 
     void calc(Integer x) {
@@ -71,7 +82,60 @@ public class Main {
     }
 
 
-     //abc
+
+
+
+        boolean isbalanced(String s) {
+            int x = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    x += 1;
+                } else {
+                    x -= 1;
+                }
+                if (x < 0) {
+                    return false;
+                }
+            }
+            if (x != 0) {
+                return false;
+            }
+            return true;
+        }
+        boolean checkLimit(String s, int A) {
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    count += 1;
+                }
+                if (count >= A) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        void generate(StringBuilder sb, int index, int len) {
+            if (index == len) {
+                if (isbalanced(sb.toString())) {
+                    System.out.println(sb.toString());
+                }
+                return;
+            }
+            if (checkLimit(sb.toString(), (len/2))) {
+                sb.setCharAt(index, '(');
+                generate(sb, index + 1, len);
+            } else {
+                sb.setCharAt(index, ')');
+                generate(sb, index + 1, len);
+            }
+        }
+    }
+
+
+
+    //abc
     //
 
 
