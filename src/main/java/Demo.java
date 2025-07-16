acea86a221fb594123ff65ebcbfc078060dac972
vaibhavgupta <vaibhavg7005@gmail.com>
2025-07-16 06:35:08 +0530

    range

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         byte b = 127 ;
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 127; i++) {
             b++;
         }
         System.out.println(b);
