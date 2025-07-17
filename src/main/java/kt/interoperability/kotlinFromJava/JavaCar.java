69bc418e426cf0f18f81ac58b8a2d9d26272df08
vaibhavgupta <vaibhavg7005@gmail.com>
2025-07-17 12:44:48 +0530

    exec

--- src/main/java/kt/interoperability/kotlinFromJava/JavaCar.java
+++ src/main/java/kt/interoperability/kotlinFromJava/JavaCar.java
@@ -12,10 +12,33 @@ public class JavaCar {
 //        System.out.println(s == b);
 
 
-        int a = 65538;
-        char c = (char)a;
-        System.out.println(c);
-        System.out.println((int) c);
+//        int a = 65538;
+//        char c = (char)a;
+//        System.out.println(c);
+//        System.out.println((int) c);
+
+        String s1 = "ans";
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            s1 = s1.concat("lobo");
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+
+
+        long startTime1 = System.currentTimeMillis();
+        StringBuffer s2 = new StringBuffer("ans");
+        for (int i = 0; i < 100000; i++) {
+            s2.append("lobo");
+        }
+        long endTime1 = System.currentTimeMillis();
+        System.out.println(endTime1 - startTime1);
+
+
+
+
+
+
 
     }
 
