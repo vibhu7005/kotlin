f47e4f454dddbaf9a0b1337c5fa396b12c9e79c7
vaibhavgupta <vaibhavg7005@gmail.com>
2025-07-17 08:26:03 +0530

    Circularity

--- src/main/java/kt/interoperability/kotlinFromJava/JavaCar.java
+++ src/main/java/kt/interoperability/kotlinFromJava/JavaCar.java
@@ -5,16 +5,26 @@ import java.sql.SQLOutput;
 
 public class JavaCar {
     public static void main(String[] args) {
-        String a = "hulk";
-        String b = "hulkman";
-        String s = a + "man";
-        String v = new String("hulkman");
-        System.out.println(s == b);
+//        String a = "hulk";
+//        String b = "hulkman";
+//        String s = a + "man";
+//        String v = new String("hulkman");
+//        System.out.println(s == b);
+
+
+        int a = 65538;
+        char c = (char)a;
+        System.out.println(c);
+        System.out.println((int) c);
+
     }
 
     void foo(Integer i ) {
         i = i + 9;
         System.out.println("foo changed value to" + i);
     }
+
+
+
 
 }
