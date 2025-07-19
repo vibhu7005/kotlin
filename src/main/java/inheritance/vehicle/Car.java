7e68398ddfdf984ee9d7afd569aef2b653b32c33
vaibhavgupta <vaibhavg7005@gmail.com>
2025-07-19 20:35:22 +0530

    immutability

--- src/main/java/inheritance/vehicle/Car.java
+++ src/main/java/inheritance/vehicle/Car.java
@@ -6,11 +6,16 @@ import kt.academy.learnprogramming.challenge2.oop.C;
 public class Car extends Vehicle {
     public String engine;
 
-    public static void fooo() {
-
+    public static void fooo(StringBuffer s) {
+        s.append("fdfd");
     }
 
     public static void main(String[] args) {
+        int x = 4;
+        StringBuffer s = new StringBuffer("hulk");
+        fooo(s);
+        System.out.println(s);
+
     }
 
 
