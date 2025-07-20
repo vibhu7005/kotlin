04926c34dff0d77a18a8f6dfc6554781ff770ecb
vaibhavgupta <vaibhavg7005@gmail.com>
2025-07-20 12:38:00 +0530

    invariance

--- src/main/java/inheritance/vehicle/Car.java
+++ src/main/java/inheritance/vehicle/Car.java
@@ -1,6 +1,10 @@
 package inheritance.vehicle;
 
 
+import kt.A;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Car extends Vehicle {
     public String engine;
@@ -10,13 +14,28 @@ public class Car extends Vehicle {
     }
 
     public static void main(String[] args) {
-        StringBuffer s = new StringBuffer("hulk");
-        Integer i =12;
-        fooo(i);
-        i = 13;
-        System.out.println(i);
+//        StringBuffer s = new StringBuffer("hulk");
+//        Integer i =12;
+//        fooo(i);
+//        i = 13;
+//        System.out.println(i);
+//        ArrayList<Integer> x = new ArrayList<>();
+//        x.add(4);
+//        List<Person> persons = new ArrayList<>();
+//        persons.add(new Student());
+
+        List<String> strings = new ArrayList<>();
+        strings.add("RAMA");
+        List<Object> objects = new ArrayList<>();
+        objects.add(4);
+        strings.addAll((List)objects);
+        System.out.println(strings.size());
     }
 
 
 
 }
+
+class Person {}
+
+class Student extends Person{}
