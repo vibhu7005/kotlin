e530723b0a9d5bd5378775beaf774cd085de75db
vaibhavgupta <vaibhavg7005@gmail.com>
2025-07-29 06:35:13 +0530

    multithreading

--- src/main/java/Threads.java
+++ src/main/java/Threads.java
@@ -5,11 +5,15 @@ public class Threads {
     public static void main(String[] args) {
         List<Thread> threadList = new ArrayList<>();
 
-        Runnable runnable = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        Runnable runnable = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("count" + i +  "  " + Thread.currentThread().getName());
+            }
+        });
 
         for (int i =0 ; i < 20; i++)
             threadList.add(new Thread(runnable));
 
-        threadList.forEach(Thread::run);
+        threadList.forEach(Thread::start);
     }
 }
