dd89096d141519140c9267e000866588658785a7
vaibhavgupta <vaibhavg7005@gmail.com>
2025-07-31 11:20:41 +0530

    Executors

--- src/main/java/multithreading/FanInOut.java
+++ src/main/java/multithreading/FanInOut.java
@@ -2,93 +2,65 @@ package multithreading;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FanInOut {
+    private int i;
+
+    FanInOut(int i) {
+        this.i = i;
+    }
+
     public static void main(String[] args) {
+
+        int i = 0;
+        Exec exec = new Exec();
+        exec.foo();
+    }
+
+
+    static class Exec {
         List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= 50; i++) {
-            list.add(i);
+
+        int i = 0;
+
+        synchronized void getAnd() {
+            while (i < 40) {
+                System.out.println(list.get(i));
+                i++;
+            }
         }
 
-        Runnable r1 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(list.get(i));
-                }
+        void foo() {
+            for (int j = 1; j <= 50; j++) {
+                list.add(j);
             }
-        };
 
-
-
-        Runnable r2 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 10; i < 20; i++) {
-                    System.out.println(list.get(i));
-                }
+            while (i < 10) {
+                System.out.println(list.get(i));
+                i++;
             }
-        };
 
-        Runnable r3 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 20; i < 30; i++) {
-                    System.out.println(list.get(i));
-                }
+            ArrayList<Thread> threads = new ArrayList<>();
+
+            for (int j = 0; j < 3; j++) {
+                threads.add(new Thread(this::getAnd));
             }
-        };
 
-        Runnable r4 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 30; i < 40; i++) {
-                    System.out.println(list.get(i));
+            threads.forEach(Thread::start);
+            threads.forEach(thread -> {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
+            });
+
+            while (i < 50) {
+                System.out.println(list.get(i));
+                i++;
             }
-        };
-        Runnable r5 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 40; i < 50; i++) {
-                    System.out.println(list.get(i));
-                }
-            }
-        };
 
-        Thread t1 = new Thread(r1);
-        Thread t2 = new Thread(r2);
-        Thread t3 = new Thread(r3);
-        Thread t4 = new Thread(r4);
-        Thread t5 = new Thread(r1);
-
-        t1.start();
-
-
-        try {
-            t1.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
-//        System.out.println("next");
-
-        t2.start();
-        t3.start();
-        t4.start();
-
-        try {
-
-            t2.join();
-            t3.join();
-            t4.join();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        for (int i = 40; i < 50; i++) {
-            System.out.println(list.get(i));
-        }
-
-
     }
 }
