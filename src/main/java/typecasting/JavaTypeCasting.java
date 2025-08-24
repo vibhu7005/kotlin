559989087c1c93b17553e2463d5ec2a7ad597674
vaibhavgupta <vaibhavg7005@gmail.com>
2025-08-24 19:49:49 +0530

    playing around

---
+++ src/main/java/typecasting/JavaTypeCasting.java
@@ -0,0 +1,13 @@
+package typecasting;
+
+public class JavaTypeCasting {
+
+    public static void main(String[] args) {
+//        int a = 'd';
+//        System.out.println(a);
+
+        byte b = (byte) 128;
+        System.out.println(b);
+
+    }
+}
