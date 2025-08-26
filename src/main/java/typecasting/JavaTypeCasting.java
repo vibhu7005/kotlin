485f54b422f52aec9a153aeb8665392f1bfe27e0
vaibhavgupta <vaibhavg7005@gmail.com>
2025-08-26 08:36:24 +0530

    unversioned

--- src/main/java/typecasting/JavaTypeCasting.java
+++ src/main/java/typecasting/JavaTypeCasting.java
@@ -6,8 +6,17 @@ public class JavaTypeCasting {
 //        int a = 'd';
 //        System.out.println(a);
 
-        byte b = (byte) 128;
-        System.out.println(b);
+//        byte b = (byte) 128;
+//        System.out.println(b);
 
+        int i = 9;
+        if (i <= 1) System.out.println("non prime");
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) {
+                System.out.println("non prime");
+                return;
+            }
+        }
+        System.out.println("prime");
     }
 }
