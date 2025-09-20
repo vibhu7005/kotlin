d9052e9838ea53d61505acfb21dd79952612a232
vaibhavgupta <vaibhavg7005@gmail.com>
2025-09-20 16:02:40 +0530

    dsa

--- src/main/java/Backtrack.java
+++ src/main/java/Backtrack.java
@@ -2,25 +2,27 @@ import java.util.ArrayList;
 
 public class Backtrack {
 
-    static ArrayList<Integer> list = new ArrayList<>();
+    static int []arr = {1,2,3};
+
     public static void main(String[] args) {
-        int []arr = {1,2,3};
-        backt(arr, 0);
+        ArrayList<Integer> list = new ArrayList<>();
+        backt(0, list, 0);
     }
 
-    static void backt(int arr[], int index) {
+    static void backt( int index, ArrayList<Integer>list, int h) {
         if (index == arr.length) {
             for (int i = 0; i < list.size(); i++) {
-                System.out.print(list.get(i)+ " ");
+                System.out.print(list.get(i) + " ");
             }
             System.out.println();
             return;
         }
 
-        for (int i = index; i < arr.length; i++) {
-            list.add(arr[i]);
-            backt(arr, index + 1);
-            list.remove(list.size()-1);
+        for (int i = h; i < arr.length; i++) {
+            list.add(arr[index]);
+            backt(index + 1, list, h);
+            list.remove(list.size() - 1);
         }
+
     }
 }
