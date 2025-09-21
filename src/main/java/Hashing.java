687a316d3d012588679da3a57f623370b1bc98b5
vaibhavgupta <vaibhavg7005@gmail.com>
2025-09-21 11:40:42 +0530

    dsa

--- src/main/java/Hashing.java
+++ src/main/java/Hashing.java
@@ -1,24 +1,32 @@
+import kotlin.Pair;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Hashing {
 
+    static ArrayList<ArrayList<Pair<Integer, Integer>>> list = new ArrayList<>();
+
+
+
+
     public static void main(String[] args) {
-        int [] arr = {2,0,1,2,0};
-        Arrays.sort(arr);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int k = 1;
-        int count =0;
-        for(int i = 0; i < arr.length; i++) {
-            if (map.containsKey(arr[i] - k)) {
-                count += map.get(arr[i]-k);
-            }
-
-            if (map.containsKey(arr[i])) {
-                map.put(arr[i], map.get(arr[i]) + 1);
-            } else map.put (arr[i], 1);
+        for (int i = 0; i< 10; i++) {
+            list.add(new ArrayList<Pair<Integer,Integer>>());
         }
+    }
 
-        System.out.println(count);
+    static int hashcode(int n)  {
+        return n % 10;
+    }
+
+    static void put (int key, int value) {
+        int hash = hashcode(key);
+        Pair<Integer,Integer> p = new Pair<>(key,value);
+        if (list.size() <
+        list.add(hash, p);
+
     }
 }
