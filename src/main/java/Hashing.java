5ccebed06e49cc87f9d8004101fb8ac3f8663317
vaibhavgupta <vaibhavg7005@gmail.com>
2025-09-25 13:14:39 +0530

    **dp**

--- src/main/java/Hashing.java
+++ src/main/java/Hashing.java
@@ -7,29 +7,40 @@ import java.util.HashMap;
 
 public class Hashing {
 
-    static int dp[] = new int[21];
+    static int lis[] = new int[11];
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
-        Arrays.fill(dp, -1);
-        System.out.println(countMin(18));
-
-        for (int i = 0; i<18; i++) {
-            System.out.println(dp[i]);
+        lis[0] = 1;
+        int arr[] = {0,8,4,12,2,10,16,1,9,5,13};
+        for (int i = 0; i < arr.length; i++) {
+            int j;
+            for (j = i-1; j >=0; j--) {
+                if (arr[j] < arr[i]) {
+                    lis[i] = lis[j]+1;
+                    break;
+                }
+            }
+            if (j == -1) {
+                lis[i] = 1;
+            }
         }
 
-    }
-
-    static int countMin(int n) {
-        if (n == 0) return 0;
-
-        if (dp[n] != -1) return dp[n];
-
-        for (int i = 1; i * i <= n; i++) {
-            int x = i * i;
-            min = Math.min(min, 1 + countMin(n - x));
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(lis[i] + " ");
         }
-        dp[n] = min;
-        return min;
     }
+
+//    static int countMin(int n) {
+//        if (n == 0) return 0;
+//
+//        if (dp[n] != -1) return dp[n];
+//
+//        for (int i = 1; i * i <= n; i++) {
+//            int x = i * i;
+//            min = Math.min(min, 1 + countMin(n - x));
+//        }
+//        dp[n] = min;
+//        return min;
+//    }
 }
