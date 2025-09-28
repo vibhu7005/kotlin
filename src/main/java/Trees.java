2c9ff6201a2d34be78f472ce1e161e1ed3e68965
vaibhavgupta <vaibhavg7005@gmail.com>
2025-09-28 09:12:34 +0530

    tr

--- src/main/java/Trees.java
+++ src/main/java/Trees.java
@@ -1,63 +1,53 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Trees {
 
-    static class TreeNode {
-        int data;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int data) {
-            this.data = data;
-        }
+    static class TrieNode {
+        boolean occurred;
+        HashMap<Character, TrieNode> map = new HashMap<>();
+
     }
 
     public static void main(String[] args) {
-        TreeNode tree = new TreeNode(34);
-        tree.left = null;
-        tree.right = new TreeNode(45);
-    }
-}
-
-/**
- * Definition for binary tree
- * class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) {
- *      val = x;
- *      left=null;
- *      right=null;
- *     }
- * }
- */
-
-public class Solution {
-    TreeMap<Integer, ArrayList<Integer>> map = new TreeMap();
-    int maxHeight = 0;
-    boolean check = true;
-    public ArrayList<ArrayList<Integer>> solve(TreeNode A) {
-        ArrayList<ArrayList<Integer>> lisi = new ArrayList();
-        inOrder(A);
-        System.out.println(check);
-        return lisi;
-    }
-
-    void inOrder(TreeNode A) {
-        if (A==null) return;
-        if (Math.abs(height(A.left) - height(A.right)) > 1) {
-            check = false;
+        String[] inputs = {"TIE", "TRAP", "PLATE", "CAT", "PART", "PLACE"};
+        TrieNode root = new TrieNode();
+        for (int i = 0; i < inputs.length; i++) {
+            TrieNode temp = root;
+            for (int j = 0; j < inputs[i].length(); j++) {
+                if (temp.map.containsKey(inputs[i].charAt(j))) {
+                    temp.map.get(inputs[i].charAt(j)).occurred = true;
+                } else {
+                    temp.map.put(inputs[i].charAt(j), new TrieNode());
+                }
+                temp = temp.map.get(inputs[i].charAt(j));
+            }
         }
-        inOrder(A.left);
-        inOrder(A.right);
+
+
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println(root.map.size());
+
+
+        for (int i = 0; i < inputs.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            TrieNode temp = root;
+            System.out.println(temp.map.size());
+            for (int j = 0; j < inputs[i].length(); j++) {
+                temp = temp.map.get(inputs[i].charAt(j));
+                sb.append(inputs[i].charAt(j));
+                if (!temp.occurred) {
+                    break;
+                }
+            }
+            list.add(sb.toString());
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
-
-    int height(TreeNode node) {
-        if (node == null) return -1;
-        int l = height(node.left);
-        int r = height(node.right);
-        return 1 + Math.max(l,r);
-    }
-
-
-
 }
+
 
