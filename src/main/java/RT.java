b4064b0401b665703e4557d1d5d2a47d59fbb1ef
vaibhavgupta <vaibhavg7005@gmail.com>
2025-10-01 12:57:23 +0530

    single

--- src/main/java/RT.java
+++ src/main/java/RT.java
@@ -5,6 +5,18 @@ public class RT {
     public static void main(String[] args) {
         Logger.getInstance().log();
         Logger.getInstance().log();
+        Naman.INSTANCE.printValue();
+    }
+}
+
+enum Naman {
+    INSTANCE(90);
+    private int value;
+    Naman(int value) {
+        this.value = value;
+    }
+    void printValue() {
+        System.out.println(value);
     }
 }
 
