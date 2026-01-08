276e983d65c6bdc0a41f0f38739de54fef83b88b
vaibhavgupta <vaibhavg7005@gmail.com>
2026-01-08 08:53:30 +0530

    strings

--- src/main/java/Demo2.java
+++ src/main/java/Demo2.java
@@ -7,7 +7,15 @@ public class Demo2 {
         double d1 = 0.1;
         double d2 = 0.2;
         double d3 = 0.3;
-        System.out.println((d1 + d2) == d3);  // prints: false
+        System.out.println((d1 + d2) == d3);
+        int x = 3;
+        fund(x);
+        System.out.println(x);
 
+
+    }
+
+     static void fund(int x) {
+        x = x + 4;
     }
 }
