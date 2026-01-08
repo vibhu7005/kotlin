993a7f20e763980a8b7dd030c2cb6eac9da0c90c
vaibhavgupta <vaibhavg7005@gmail.com>
2026-01-08 08:07:25 +0530

    java

--- src/main/java/Demo.kt
+++ src/main/java/Demo.kt
@@ -1,12 +1,21 @@
+import kotlin.reflect.typeOf
+
 class Demo {
     val df : Int = 56
 
 
 }
 
+open class Animal
+class Dog : Animal()
+
 fun main(args: Array<String>) {
-    var a : Byte = 129.toByte()
-    var b = 45
-    var c: Byte = b.toByte()
-    println(a)
+    var a = 256
+    var d : Byte = a.toByte()
+    println(d)
+    val animal : Animal = Dog();
+    val lok  = 1000000000000
+    val chad = 'a'
+    val s : Int = chad.code
+//    println(Char.MAX_HIGH_SURROGATE)
 }
--- src/main/java/Demo2.java
+++ src/main/java/Demo2.java
@@ -4,5 +4,10 @@ public class Demo2 {
         int a = -256;
         byte b = (byte)a;
         System.out.println(b);
+        double d1 = 0.1;
+        double d2 = 0.2;
+        double d3 = 0.3;
+        System.out.println((d1 + d2) == d3);  // prints: false
+
     }
 }
