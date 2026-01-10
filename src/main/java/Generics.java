85e5a67751b81eb3be9ee4237f8442ab5c7c915e
vaibhavgupta <vaibhavg7005@gmail.com>
2026-01-10 16:16:14 +0530

    generics covariance

--- src/main/java/Generics.java
+++ src/main/java/Generics.java
@@ -1,14 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generics {
     public static void main(String[] args) {
         Xyz obj = new Xyz("abc", 34);
         modify(obj);
         System.out.println(obj.second);
+        List<Dog> dogList = new ArrayList<>();
+        Dog dog = new Dog();
+        acceptAnimals(dogList);
+        acceptAnimal(dog);
     }
 
     static void modify(Xyz x) {
         x.second = "lop";
     }
+
+    //covariance
+    static void acceptAnimals(List<? extends Animal> animalList) {
+        Animal dog = new Animal();
+    }
+
+    static void acceptAnimal(Animal animal) {
+
+    }
+
+
+
 }
+
+class Cat extends Animal{}
+
 
 class Xyz<T,E> {
     final T first;
